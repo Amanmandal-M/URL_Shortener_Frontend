62b886e76054b86ebf6545fe57b7d065ce70e926
Amanmandal-M <amanmandal644@gmail.com>
2023-08-10 16:13:55 +0530

    Fix bugs

--- js/index.js
+++ js/index.js
@@ -6,158 +6,99 @@ const shortButton = document.getElementById("shortButton");
 const appendUrl = document.querySelector("#appendShortUrl");
 const originalUrl = document.querySelector(".originalUrl");
 
-shortButton.addEventListener("click", (e) => {
-  e.preventDefault();
+function shortenUrl() {
+    if (originalUrl.value == "")
+        return swal.fire({
+            icon: "info",
+            title: "Enter URL First",
+            width: "auto",
+        });
+    let obj = { originalUrl: originalUrl.value };
 
-  validateInput();
+    let data = originalUrl.value,
+        flag = false,
+        bag = "";
 
-  if (originalUrl.value == "")
-    return swal.fire({
-      icon: "info",
-      title: "Enter URL First",
-      width: "auto",
-    });
-  let obj = { originalUrl: originalUrl.value };
-
-  let data = originalUrl.value,
-    flag = false,
-    bag = "";
-
-  for (let i = 0; i <= 4; i++) {
-    bag += data[i];
-    if (bag == "http" || bag == "https") {
-      flag = true;
-      break;
+    for (let i = 0; i <= 4; i++) {
+        bag += data[i];
+        if (bag == "http" || bag == "https") {
+            flag = true;
+            break;
+        }
     }
-  }
 
-  if (flag) {
-    shortButton.innerHTML = `<div class="spinner"></div>`;
-    postData(obj);
-  } else
-    return swal.fire({
-      icon: "info",
-      title: "You are not using valid URL!",
-      width: "auto",
-    });
+    if (flag) {
+        shortButton.innerHTML = `<div class="spinner"></div>`;
+        postData(obj);
+    } else
+        return swal.fire({
+            icon: "info",
+            title: "You are not using a valid URL!",
+            width: "auto",
+        });
+}
+
+// Event listener for "Shorten URL" button click
+shortButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    shortenUrl();
+});
+
+// Event listener for Enter key press on originalUrl input
+originalUrl.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        shortenUrl();
+    }
 });
 
 const postData = async (data) => {
-  try {
-    const apiRes = await fetch(postUrl, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const dataRes = await apiRes.json();
+    try {
+        const apiRes = await fetch(postUrl, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const dataRes = await apiRes.json();
 
-    let shortUrl = dataRes.data.shortUrl;
-    if (apiRes.status == 201) {
-      const ipAPI = `${baseUrl}/${shortUrl}`;
-      shortButton.textContent = "Shorten URL";
+        let shortUrl = dataRes.data.shortUrl;
+        if (apiRes.status == 201) {
+            const ipAPI = `${baseUrl}/${shortUrl}`;
+            shortButton.textContent = "Shorten URL";
 
-      Swal.queue([
-        {
-          icon: "success",
-          title: "Shorted URL",
-          confirmButtonText: "Show my Shorted URL",
-          showLoaderOnConfirm: true,
-          preConfirm: () => {
-            return ipAPI
-              ? (Swal.insertQueueStep({
-                  title: ipAPI,
-                  width: "auto",
-                  color: "blue",
-                  confirmButton: true,
-                }),
-                (originalUrl.value = ""))
-              : +Swal.insertQueueStep({
-                  icon: "error",
-                  title: "Unable to get your URL",
-                  width: "auto",
-                });
-          },
-        },
-      ]);
+            Swal.queue([
+                {
+                    icon: "success",
+                    title: "Shorted URL",
+                    confirmButtonText: "Show my Shorted URL",
+                    showLoaderOnConfirm: true,
+                    preConfirm: () => {
+                        return ipAPI
+                            ? (Swal.insertQueueStep({
+                                title: ipAPI,
+                                width: "auto",
+                                color: "blue",
+                                confirmButton: true,
+                            }),
+                                (originalUrl.value = ""))
+                            : +Swal.insertQueueStep({
+                                icon: "error",
+                                title: "Unable to get your URL",
+                                width: "auto",
+                            });
+                    },
+                },
+            ]);
+        }
+    } catch (error) {
+        swal.fire({
+            icon: "error",
+            title: "Internal error 500",
+            width: "auto",
+        });
+        shortButton.textContent = "Shorten URL";
+        width: "auto";
     }
-  } catch (error) {
-    swal.fire({
-      icon: "error",
-      title: "Internal error 500",
-      width: "auto",
-    });
-    shortButton.textContent = "Shorten URL";
-    width: "auto";
-  }
-};
-
-function validateInput() {
-  let input = document.getElementById("myInput").value;
-  let pattern = /^[0-9a-zA-Z]{5}$/;
-
-  if (input == "")
-    return swal.fire({
-      icon: "info",
-      title: "Enter URL First",
-      width: "auto",
-    });
-
-  if (pattern.test(input)) {
-    alert("Input is valid.");
-  } else {
-    alert("Input is invalid.");
-  }
-}
-
-const postDataAliasData = async (data) => {
-  try {
-    const apiRes = await fetch(postUrl, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const dataRes = await apiRes.json();
-
-    let shortUrl = dataRes.data.shortUrl;
-    if (apiRes.status == 201) {
-      const ipAPI = `${baseUrl}/${shortUrl}`;
-      shortButton.textContent = "Shorten URL";
-
-      Swal.queue([
-        {
-          icon: "success",
-          title: "Shorted URL",
-          confirmButtonText: "Show my Shorted URL",
-          showLoaderOnConfirm: true,
-          preConfirm: () => {
-            return ipAPI
-              ? (Swal.insertQueueStep({
-                  title: ipAPI,
-                  width: "auto",
-                  color: "blue",
-                  confirmButton: true,
-                }),
-                (originalUrl.value = ""))
-              : +Swal.insertQueueStep({
-                  icon: "error",
-                  title: "Unable to get your URL",
-                  width: "auto",
-                });
-          },
-        },
-      ]);
-    }
-  } catch (error) {
-    swal.fire({
-      icon: "error",
-      title: "Internal error 500",
-      width: "auto",
-    });
-    shortButton.textContent = "Shorten URL";
-    width: "auto";
-  }
 };
